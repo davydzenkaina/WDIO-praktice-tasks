a52ac6bef55d21217af970fb968969ed100274fa
Ina Davydzenka <davydzenkaina@gmail.com>
2022-10-06 17:57:28 +0300

    Apdate Pastebin test

--- test/specs/practice-task1.js
+++ test/specs/practice-task1.js
@@ -1,14 +1,13 @@
-const Pastebin = require ('../pageobjects/pastebin.page');
+const Pastebin = require('../pageobjects/pastebin.page');
 
-describe('Should create New Paste on website Pastebin.com ',  () => {
-    it ('Should create New Paste on website Pastebin.com with next params', async () => {
-      Pastebin.open()
-    await browser.pause(5000)
+describe('Should create New Paste on website Pastebin.com ', () => {
+  it('Should create New Paste on website Pastebin.com with next params', async () => {
+    Pastebin.open()
+    browser.setWindowSize(1920, 1080)
     await Pastebin.createCode.addValue('Hello from WebDriver')
     await Pastebin.openPasteExpiration.click()
     await Pastebin.shoose10Minutes.click()
     await Pastebin.openPasteName.addValue('helloweb')
-    await browser.pause(5000)   
-    })
+  })
 })
 
