bc3d316aad0ffa30ff6b792368f362aebba24a8c
Ina Davydzenka <davydzenkaina@gmail.com>
2022-10-06 18:10:57 +0300

    Add Pastebin task2

--- test/pageobjects/pastebin.page.js
+++ test/pageobjects/pastebin.page.js
@@ -2,33 +2,35 @@
 
 const Page = require('./page');
 
-
 class Pastebin extends Page {
 
     get openPasteName() {
         return $('#postform-name')
     }
-
+    get checkPasteName() {
+        return $('div.info-top')
+    }
     get openPasteExpiration() {
         return $('#select2-postform-expiration-container');
-
     } 
     get openSyntaxHighlighting() {
         return $('#select2-postform-format-container');
     } 
-
     get shoose10Minutes() {
         return $(`//li[text()='10 Minutes']`);
     }
-
     get createCode() {
         return $('#postform-text')
     }
-
+    get checkCode() {
+        return $('div.source bash')
+    }
     get chooseBash() {
         return $(`//li[text()='Bash']`)
     }
-
+    get checkBash() {
+        return $('a.btn -small.h_800')
+    }
 get saveNewPaste(){
     return $('//*[text()="Create New Paste"]')
 }
